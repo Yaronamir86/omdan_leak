8df74d6889c43f57d1167475c06546fee0309ed7
yaron amir <yaronamir86@gmail.com>
2026-04-12 11:26:52 +0300

    editing leak case equal to property

---
+++ hosting/app/config/firebase-config-global.js
@@ -0,0 +1,10 @@
+// OMDAN LEAK – global Firebase config for compat SDK pages
+window.FIREBASE_CONFIG = {
+  apiKey: "AIzaSyAo5atj2i8RgevguKGR662-IQnjl1R45yI",
+  authDomain: "omdan-leak.firebaseapp.com",
+  projectId: "omdan-leak",
+  storageBucket: "omdan-leak.firebasestorage.app",
+  messagingSenderId: "979018499185",
+  appId: "1:979018499185:web:1496303b1cd8a993bdc7d0",
+  measurementId: "G-P62VFXD521"
+};
---
+++ hosting/app/config/firebase-config.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+export const firebaseConfig = {
+  apiKey: "AIzaSyAo5atj2i8RgevguKGR662-IQnjl1R45yI",
+  authDomain: "omdan-leak.firebaseapp.com",
+  projectId: "omdan-leak",
+  storageBucket: "omdan-leak.firebasestorage.app",
+  messagingSenderId: "979018499185",
+  appId: "1:979018499185:web:1496303b1cd8a993bdc7d0",
+  measurementId: "G-P62VFXD521"
+};
